d5e07af86f847f41f057640f986c292a1133eef6
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2025-08-16 10:50:50 +0200

    Add core concepts component to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import imgReact from './assets/react-core-concepts.png';
+import imgComponents from './assets/components.png';
+
 const reactDescriptions = ["Core", "Fundamental", "Crucial"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
+function CoreConcept(prompts) {
+  return (
+    <li>
+      <img src={prompts.img} alt={prompts.title} />
+      <h3>{prompts.title}</h3>
+      <p>{prompts.description}</p>
+    </li>
+  )
+}
+
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={imgReact} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
@@ -22,7 +35,19 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              img={imgComponents}
+              description="Core UI building block."
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
