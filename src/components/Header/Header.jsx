a641d94e079eb13f2d66a607c006dead6343d2c1
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2025-08-17 12:10:21 +0200

    Reorganize Header code component and its styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import imgReact from '../assets/react-core-concepts.png';
+import imgReact from '../../assets/react-core-concepts.png';
+import './Header.css';
 
 const reactDescriptions = ["Core", "Fundamental", "Crucial"];
 
